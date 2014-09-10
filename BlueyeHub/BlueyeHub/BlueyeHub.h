5dbff5379123264646fb092cef78d99e8b37e5f3
DJ Blueye <djblueye@DJs-MacBook-Pro.local>
2014-09-10 10:42:43 -0500

    sdk updates

---
+++ BlueyeHub/BlueyeHub/BlueyeHub.h
@@ -0,0 +1,14 @@
+//
+//  BlueyeHub.h
+//  BlueyeHub
+//
+//  Created by DJ Blueye on 9/9/14.
+//  Copyright (c) 2014 Blueye. All rights reserved.
+//
+
+#ifndef BlueyeHub_BlueyeHub_h
+#define BlueyeHub_BlueyeHub_h
+
+#import "HUB.h"
+
+#endif
---
+++ BlueyeHub/BlueyeHubTests/BlueyeHubTests.m
@@ -0,0 +1,34 @@
+//
+//  BlueyeHubTests.m
+//  BlueyeHubTests
+//
+//  Created by DJ Blueye on 9/9/14.
+//  Copyright (c) 2014 Blueye. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BlueyeHubTests : XCTestCase
+
+@end
+
+@implementation BlueyeHubTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
